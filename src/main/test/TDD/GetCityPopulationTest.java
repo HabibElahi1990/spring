5b73d00ae66347b9b27f37800d7e499b8e6f2255
h.elahi <habib.elahi1990@gmail.com>
2020-10-29 23:00:46 +0330

    TDD test example

---
+++ src/main/test/TDD/GetCityPopulationTest.java
@@ -0,0 +1,48 @@
+package TDD;
+
+import org.junit.Before;
+import org.junit.Test;
+import springCore.TDDService.CityPopulationService;
+
+import static org.junit.Assert.assertEquals;
+
+public class GetCityPopulationTest {
+    CityPopulationService cityPopulationService=null;
+
+    @Before
+    public void createObjectInstance(){
+        cityPopulationService=new CityPopulationService();
+    }
+
+
+    @Test
+    public void getCityPopulation() {
+        String cityName = "Isfahan";
+        Integer expectedCityPopulationCount = 2000000;
+        Integer cityPopulationCount = cityPopulationService.getCityPopulation(cityName);
+        assertEquals(cityPopulationCount, expectedCityPopulationCount);
+    }
+
+    @Test
+    public void getCityPopulationEmptyValue() {
+        String cityName = "";
+        Integer expectedCityPopulationCount = 0;
+        Integer cityPopulationCount = cityPopulationService.getCityPopulation(cityName);
+        assertEquals(cityPopulationCount, expectedCityPopulationCount);
+    }
+
+    @Test
+    public void getCityPopulationNullValue() {
+        String cityName = null;
+        Integer expectedCityPopulationCount = 0;
+        Integer cityPopulationCount = cityPopulationService.getCityPopulation(cityName);
+        assertEquals(cityPopulationCount, expectedCityPopulationCount);
+    }
+    @Test
+    public void getCityPopulationInvalidValue() {
+        String cityName = "fasghdgha";
+        Integer expectedCityPopulationCount = 0;
+        Integer cityPopulationCount = cityPopulationService.getCityPopulation(cityName);
+        assertEquals(cityPopulationCount, expectedCityPopulationCount);
+    }
+}
